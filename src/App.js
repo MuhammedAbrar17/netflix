021951f47e415abe89f341aa66aaf748dd3fe40c
Abrar <abuabrar520724@gmail.com>
2023-10-31 19:36:30 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {originals,action} from './urls'
 import Navbar from './components/Navbar/navbar';
+import Banner from './components/Banner/Banner';
+import Rowpost from './components/Rowpost/Rowpost';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Banner/>
+      <Rowpost url={originals} title='Netflix Originals'/>
+      <Rowpost url={action} title='Action' isSmall/>
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = "https://api.themoviedb.org/3";
+export const API_KEY ='62d45d9658ce9101a5d27afa1daa4597';
+export const imageUrl ='https://image.tmdb.org/t/p/original'
